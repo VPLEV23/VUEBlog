a3960460a835be4d0d44faef90447d03ac9aa280
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2021-12-22 22:15:01 +0200

    Login, Register,Reset Password Components
    This isLogin, Register,Reset Password  Components Part 1

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Blogs from "../views/Blogs.vue";
-
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -12,7 +14,7 @@ const routes = [
     component: Home,
     meta:{
       title: "Home"
-    }
+    },
   },
   {
     path: "/blogs",
@@ -20,7 +22,31 @@ const routes = [
     component: Blogs,
     meta:{
       title: "Blogs"
-    }
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta:{
+      title: "Login"
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta:{
+      title: "Register"
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta:{
+      title: "ForgotPassword"
+    },
   },
 ];
 
