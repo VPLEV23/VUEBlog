6028fd1eccbb61efc4ccb0e12dcff852cd117fd9
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-01-03 19:47:23 +0200

    Profile View

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Blogs from "../views/Blogs.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import ForgotPassword from "../views/ForgotPassword.vue";
+import Profile from "../views/Profile.vue";
+import Admin from "../views/Admin.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -48,6 +50,22 @@ const routes = [
       title: "ForgotPassword"
     },
   },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+    meta:{
+      title: "Profile"
+    },
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+    meta:{
+      title: "Admin"
+    },
+  },
 ];
 
 const router = new VueRouter({
@@ -57,10 +75,10 @@ const router = new VueRouter({
 });
 
 
-// router.beforeEach((to, from, next)=>{
-//   document.title = `${to.meta.title} | VueBlog`;
-//   next();
-// })
+router.beforeEach((to, from, next)=>{
+  document.title = `${to.meta.title} | VueBlog`;
+  next();
+})
 
 // router.beforeEach((to, from, next) => {
 //   document.title = `${to.meta.title} | VueBlog`;
